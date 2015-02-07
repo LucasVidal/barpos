0ff2afad7801e7fbfa30e47598b91868ee0171ba
Lucas Vidal <lucasvidalutn@gmail.com>
2015-02-07 15:38:30 -0300

    add associations

--- app/models/modifier.rb
+++ app/models/modifier.rb
@@ -1,2 +1,4 @@
 class Modifier < ActiveRecord::Base
+	has_many :product_modifiers
+	has_many :products, through: :product_modifiers
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ActiveRecord::Base
+	has_many :order_lines
+	has_many :orders, through: :order_lines
+
+	has_many :product_modifiers
+	has_many :modifiers, through: :product_modifiers
 end
